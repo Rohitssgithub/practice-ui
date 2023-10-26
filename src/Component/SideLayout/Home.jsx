1c99e7a8a9d36f2bfaa6acb735fc95839469c66a
rohit <rohitsawant@gmail.com>
2023-10-26 09:55:02 +0530

    according using react js practice

---
+++ src/Component/SideLayout/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Accordin from '../Accordin/Accordin';
+
+const Home = () => {
+  return (
+    <>
+      Home
+      <Accordin></Accordin>
+    </>
+  )
+}
+
+export default Home
